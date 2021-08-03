f4550b3f725951ea3eee34c2faede5277ab14e67
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-03 13:54:59 +0000

    error fixed we move

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-    return ( <h1> Hello world </h1>);
+    return (
+            <div>
+                <h1>Random quote machine</h1>
+            </div>
+        );
+   
     }
 
     export default App;
