88a8db6fba43563fe0caaf23550e6d45154b4c47
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-04 13:49:53 +0000

    done with the project,left with the colours

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import {useState, useEffect, useRef} from 'react';
+import {useState, useEffect} from 'react';
 
 
 function App() {
     const [quotes , setQuotes] = useState([]); // state to handle the quotes
-    const [randomQuote, setRandomQuote] = useState('');
+    const [randomQuote, setRandomQuote] = useState([]); // state to get random quotes
+    const twitterLink =  'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+    encodeURIComponent('"' + randomQuote.quote + '" ' + randomQuote.author);
+
     const APIURL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
     //API CALL
@@ -37,7 +40,7 @@ function App() {
             </div>}
            {quotes.length !==0 && <div className="quote-author" style={{opacity: '1'}}>- <span id="author">{randomQuote.author}</span></div>}
             <div className="buttons">
-                <a className="button" id="tweet-quote" title="Tweet this quote!" target="_top" href="https://twitter.com/intent/tweet?hashtags=quotes&amp;related=freecodecamp&amp;text=%22I%E2%80%99ve%20learned%20that%20people%20will%20forget%20what%20you%20said%2C%20people%20will%20forget%20what%20you%20did%2C%20but%20people%20will%20never%20forget%20how%20you%20made%20them%20feel.%22%20Maya%20Angelou" style={{backgroundColor: "rgb(9, 242, 250)"}}>
+                <a className="button" id="tweet-quote" title="Tweet this quote!" target="_top" href={twitterLink} style={{backgroundColor: "rgb(9, 242, 250)"}}>
                     <i className="fa fa-twitter"></i><span>Tweet</span>
                 </a>
                 <button className="button" id="new-quote" style={{backgroundColor: "rgb(9, 242, 250)"}} onClick={handleRandomQuotes}>New quote</button>
