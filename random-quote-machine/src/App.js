f407aa6618da30c5034777f2e2e121f7eb12be57
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-04 09:49:03 +0000

    done with fetching quotes

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -4,8 +4,6 @@ import {useState, useEffect} from 'react';
 
 function App() {
     const [quotes , setQuotes] = useState([]); // state to handle the quotes
-    let currentQuote = '';
-    let currentAuthor ='';
     const APIURL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
     //API CALL
@@ -16,32 +14,30 @@ function App() {
         })
         .then(data=>{
             setQuotes(data.quotes);
-            console.log("quotes: ", quotes);
+            getRandomQuotes()
            
-        })
-        .then(()=>{
-            getRandomQuotes();
         })
 
     },[quotes.length]);
+
     // Get random quotes from the api
     const getRandomQuotes = ()=>{
-    const randomIndex = Math.floor(Math.random()*quotes.length);
-    currentQuote = quotes[randomIndex].quote;
-    currentAuthor = quotes[randomIndex].author;
-    console.log('index: ', randomIndex );
-    console.log('quote: ', currentQuote );
-    console.log('author: ', currentAuthor );
+    if (quotes.length!==0){
+        const randomIndex = Math.floor(Math.random()*quotes.length);
+        return quotes[randomIndex];
     }
+    }
+ 
 
-    
+
     return (
     <div id="wrapper">
         <div id="quote-box">
+        { quotes.length !==0 &&
             <div className="quote-text" style={{opacity: '1'}}>
-                 <i className="fa fa-quote-left"> </i><span id="text"> I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.</span>
-            </div>
-            <div className="quote-author" style={{opacity: '1'}}>- <span id="author">Maya Angelou</span></div>
+               <i className="fa fa-quote-left"> </i><span id="text">{getRandomQuotes().quote}</span>
+            </div>}
+           {quotes.length !==0 && <div className="quote-author" style={{opacity: '1'}}>- <span id="author">{getRandomQuotes().author}</span></div>}
             <div className="buttons">
                 <a className="button" id="tweet-quote" title="Tweet this quote!" target="_top" href="https://twitter.com/intent/tweet?hashtags=quotes&amp;related=freecodecamp&amp;text=%22I%E2%80%99ve%20learned%20that%20people%20will%20forget%20what%20you%20said%2C%20people%20will%20forget%20what%20you%20did%2C%20but%20people%20will%20never%20forget%20how%20you%20made%20them%20feel.%22%20Maya%20Angelou" style={{backgroundColor: "rgb(9, 242, 250)"}}>
                     <i className="fa fa-twitter"></i><span>Tweet</span>
