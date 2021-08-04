c8003bbba408a1f33d7fdd7944e8b2fbab4ab4db
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-04 00:02:22 +0000

    added the useState and useEffect hooks

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import {useState, useEffect} from 'react';
 
 
 function App() {
+    const [quote , setQuote] = useState([]); // state to handle the quotes
+    const [index, setIndex] = useState(0);
+    const APIURL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+
+    //API CALL
+    useEffect(() => {
+
+    })
+    // Get random quotes from the api
+    const randomQuotes = ()=>{
+
+
+    }
     return (
     <div id="wrapper">
         <div id="quote-box">
