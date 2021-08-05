19cb7c6258effbf0d831943a4fcce58a99c210fc
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-05 16:25:52 +0000

    done with the project

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -36,16 +36,16 @@ Your content can | be here, and it | can be here....
 And here. | Okay. | I think we get it.
 
 - And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
+- list 1.
+- list 2.
+- list 3.
 
 
 1. And there are numbered lists too.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
 
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+![React Logo](data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K)
 `;
 function App() {
   const [markdown, setMarkdown] = useState(text)
@@ -57,7 +57,8 @@ function App() {
     <main className="App">
       <div className="editor-container">
        <div className="editor-header">
-        <h3 className="heading">Editor</h3>
+
+        <h3 className="heading"><i className="fa fa-pencil-square-o" aria-hidden="true"></i>Editor</h3>
        </div>
       <textarea id="editor" type="text" value={markdown} onChange={handleChange}>
 
@@ -65,8 +66,8 @@ function App() {
     
     </div>
      <div className="preview-container">
-     <div className="preview-header">
-        <h3 className="heading">Previewer</h3>
+      <div className="preview-header">
+        <h3 className="heading"><i className="fa fa-refresh" aria-hidden="true"></i>Previewer</h3>
        </div>
       <div id="preview" dangerouslySetInnerHTML={{
           __html: marked(markdown,{breaks: true})}}>
