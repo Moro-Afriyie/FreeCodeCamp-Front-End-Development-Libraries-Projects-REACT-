402f96e98dddd5b17e00bc7a9340e271e1b3e468
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-05 14:04:06 +0000

    added the various packages

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
+import './App.scss';
 import marked from 'marked';
 
 //Allows line breaks
 marked.setOptions({
   breaks: true,
-  highlight: function (code) {
-    return Prism.highlight(code, Prism.languages.javascript, 'javascript');
-  }
+  // highlight: function (code) {
+  //   return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+  // }
 });
 
 function App() {
   
   const text = '## Marked in the browser\n\nRendered by **marked**.';
   return (
-    <div className="App" dangerouslySetInnerHTML={{
-        __html: marked(text))>
-    </div>
+    <main className="App">
+      
+      <div id="preview" dangerouslySetInnerHTML={{
+          __html: marked(text)}}>
+      </div>
+    </main>
   );
 }
 
