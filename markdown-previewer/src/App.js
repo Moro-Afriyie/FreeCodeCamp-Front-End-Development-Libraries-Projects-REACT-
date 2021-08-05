5b9560c638d0ce88cf00062e8701ecaec946f951
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-05 13:01:46 +0000

    added the marked library

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import marked from 'marked';
+
+//Allows line breaks
+marked.setOptions({
+  breaks: true,
+  highlight: function (code) {
+    return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+  }
+});
 
 function App() {
+  
+  const text = '## Marked in the browser\n\nRendered by **marked**.';
   return (
-    <div className="App">
+    <div className="App" dangerouslySetInnerHTML={{
+        __html: marked(text))>
     </div>
   );
 }
