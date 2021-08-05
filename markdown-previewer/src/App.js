d0e85709c82327c13b4442c5c1f57a487db8f066
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-05 14:22:27 +0000

    styled the component

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -14,7 +14,15 @@ function App() {
   const text = '## Marked in the browser\n\nRendered by **marked**.';
   return (
     <main className="App">
-      
+      <div class="editor-container">
+       <div class="editor-header">
+        <h3>Editor</h3>
+       </div>
+      <textarea id="editor" type="text" value="moro">
+
+      </textarea>
+    
+    </div>
       <div id="preview" dangerouslySetInnerHTML={{
           __html: marked(text)}}>
       </div>
