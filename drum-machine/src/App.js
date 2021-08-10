a0b4d71e9708ce217cdc2d5ab7d4bcb2edd05131
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-10 00:25:09 +0000

    added the audiio objects

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,12 +1,68 @@
 import './App.css';
 
-const clips = ['A','B','C','D','E','F']
+const bankOne = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];
+
 function App() {
   return (
     <main className="App">
       <h1 className="title">Drum Machine</h1>
       <div className="drum-container">
-        <DrumPads clips={clips}/>
+        <DrumPads clips={bankOne}/>
       </div>
     </main>
   );
@@ -14,11 +70,20 @@ function App() {
 
 
 const DrumPads = ({clips}) => {
+
+  const handlePlayAudio = () =>{
+    console.log("button clicked");
+  }
+
   return ( 
       <div className="drum-pads">
   {clips.map(key => {
     return (
-      <button key={key} className="btn">
+      <button
+       key={key}
+       className="btn"
+       onClick={handlePlayAudio}
+       >
       {key}
       <audio src=""></audio>
       </button>
