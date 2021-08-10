ab780cbb5a97870cc227a27855c7f486a52dd49c
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-10 15:53:09 +0000

    added the toggle function to switch between banks

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -58,19 +58,93 @@ const bankOne = [
   }
 ];
 
+const bankTwo = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Chord-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Chord-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Chord-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Shaker',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: 'Punchy-Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Side-Stick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Snare',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+  }
+];
+
+
 function App() {
   const [volume, setVolume] = useState(1);
-  const [audioID, setaudioID] = useState("")
+  const [audioID, setaudioID] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [banks, setBanks] = useState(bankOne);
+  // Smooth Piano Kit
+  // Heater Kit
 
+  useEffect(()=>{
+    if(toggle){
+      setBanks(bankTwo);
+    }
+    else{
+      setBanks(bankOne)
+    }
+    console.log(banks);
+  },[toggle])
 
   const handleVolumeChange=(e)=>{
     setVolume(e.target.value);
+  }
+  const handleToggle =(e)=>{
+    setToggle(e.target.checked);
   }
   return (
     <main className="App">
       <h1 className="title">Drum Machine</h1>
       <div className="drum-container">
-        {bankOne.map((clip)=>{
+        {banks.map((clip)=>{
              return <DrumPads 
              key={clip.id} 
              clip={clip}
@@ -80,6 +154,7 @@ function App() {
       </div>
       <div className="display-audio">
         <h3>{audioID}</h3>
+        <h3>{`toggle: ${toggle}`}</h3>
       </div>
       <div className="volume-slider">
       <input
@@ -90,9 +165,9 @@ function App() {
        max="1" 
        value={volume} />
       </div>
-      <label class="toggle">
-        <input type="checkbox" id="cToggle"/>
-        <span class="slider"></span>
+      <label className="toggle">
+        <input type="checkbox" checked={toggle} onChange={handleToggle} id="cToggle"/>
+        <span className="slider"></span>
       </label>
     </main>
   );
