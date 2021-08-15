b13d8d12b25e30d8610cf4c5b6d980ea296b1a7e
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-15 20:23:45 +0000

    created a dummy template to see if it will work

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -3,16 +3,21 @@ import {useState} from 'react';
 
 function App() {
   const [result, setResult] = useState("");
+  const [expression, setExpression] = useState("")
 
  const handleClick = (value)=>{
-   setResult(value);
-   console.log(result);
+  setExpression(prev=>prev + value);
+ }
+
+ const handleCalculate = ()=>{
+  setResult(eval(expression))
  }
   return (
     <main>
       <div className="App">
         <button onClick={()=> handleClick("1")}>1</button>
         <button onClick={()=> handleClick("+")}>+</button>
+        <h1>{expression}</h1>
       </div>
     </main>
   );
