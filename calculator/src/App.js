f77c40bb00ddc010377098b030aeb9f7627f0457
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-15 19:29:33 +0000

    added some dummy data

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [result, setResult] = useState("");
+
+ const handleClick = (value)=>{
+   setResult(value);
+   console.log(result);
+ }
   return (
     <div className="App">
-     
+     <button onClick={()=> handleClick("1")}>1</button>
+     <button onClick={()=> handleClick("+")}>+</button>
     </div>
   );
 }
