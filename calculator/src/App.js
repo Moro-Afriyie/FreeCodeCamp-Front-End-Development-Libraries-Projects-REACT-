9abca4b98430a2efe25a8cae11bb2f9f9d239fb6
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-08-17 10:10:10 +0000

    added a function to do the calculations

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState(0);
   const [expression, setExpression] = useState("")
 
  const handleClick = (value)=>{
@@ -10,14 +10,18 @@ function App() {
  }
 
  const handleCalculate = ()=>{
+  setExpression(prev=>prev + "=");
   setResult(eval(expression))
  }
   return (
     <main>
       <div className="App">
         <button onClick={()=> handleClick("1")}>1</button>
+        <button onClick={()=> handleClick("2")}>1</button>
         <button onClick={()=> handleClick("+")}>+</button>
+        <button onClick={()=> handleCalculate()}>=</button>
         <h1>{expression}</h1>
+        <h2>{result}</h2>
       </div>
     </main>
   );
