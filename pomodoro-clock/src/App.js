ba50ab1a0a8ccebe2497f803cd2e2d79825791d1
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-09-22 15:09:09 +0000

    implemented the controlTimer function but encountering some bugs

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [sessionLength, setSessionLength] = useState(25);
   const [displayTime, setDisplayTime] = useState(25*60); // 25 minutes
   const [TimerOn, setTimerOn] = useState(false);
+  const [onBreak, setOnBreak] = useState(false);
 
   const formatTime = (time)=>{
     let minutes = Math.floor(time / 60);
@@ -35,12 +36,30 @@ function App() {
   }
 
   const controlTimer =()=>{
+  let second = 1000;
+  let date = new Date().getTime(); // get the current date in seconds
+  let nextDate = new Date().getTime + second;
+  let onBreakVariable = onBreak;
+  if(!TimerOn){
 
+    let interval = setInterval(()=>{
+      date = new Date().getTime();
+      if(date > nextDate){
+        setDisplayTime(prev => {
+          return prev-1;
+        });
+        nextDate += second;
+      }
+    },30) //update it every 30 milliseconds
+  }
   }
 
-  const handleResetTime = ()=>[
-    
-  ]
+  const handleResetTime = ()=>{
+  setDisplayTime(25*60);
+  setBreakLength(5);
+  setSessionLength(25);
+  }
+
   return (
     <main className="container">
       <section className="time-controls">
@@ -64,9 +83,9 @@ function App() {
       <h1 className="title">Pomodoro Clock</h1>
       <section className="App">
        <h1>{formatTime(displayTime)}</h1>
-       <div className="control-buttons" onClick={controlTimer}>
-         {TimerOn? <button>Pause</button> : <button>Play</button>}
-       </div>
+       <button className="control-buttons" onClick={controlTimer}>
+         {TimerOn? "Pause": "Play"}
+       </button>
        <div className="reset-button">
          <button onClick={handleResetTime}>reset</button>
        </div>
