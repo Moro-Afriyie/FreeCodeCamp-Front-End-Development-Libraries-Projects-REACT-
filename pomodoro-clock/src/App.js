1ad84642f891c8b20dc103a2241db7700bdab4d5
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-09-22 14:16:59 +0000

    added a function to handle the break and session length

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-  const [breakLength, setBreakLength] = useState();
-  const [sessionLength, setSessionLength] = useState();
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
   const [displayTime, setDisplayTime] = useState(25*60); // 25 minutes
 
   const formatTime = (time)=>{
@@ -12,8 +12,38 @@ function App() {
     return `${minutes> 10 ? minutes : "0" + minutes}:${seconds> 10 ? seconds : "0" + seconds}` 
   }
  
+  const handleChangeBreakLength = (number, type)=>{
+    if(type==="break"){
+      setBreakLength(prev => prev + number);
+     
+    }
+    else{
+      setSessionLength(prev => prev + number);
+    }
+     
+  }
+
+
   return (
     <main className="container">
+      <section className="time-controls">
+        <div id="break-label">
+          <p>Break Length</p>
+          <div className="button-container">
+            <button onClick={()=> handleChangeBreakLength(-1,"break")}>-</button>
+            <p>{breakLength}</p>
+            <button onClick={()=> handleChangeBreakLength(1,"break")}>+</button>
+          </div>
+        </div>
+        <div id="section-label">
+          <p>Session Length</p>
+          <div className="button-container">
+            <button >-</button>
+            <p>{sessionLength}</p>
+            <button>+</button>
+          </div>
+        </div>
+      </section>
       <h1 className="title">Pomodoro Clock</h1>
       <section className="App">
        <h1>{formatTime(displayTime)}</h1>
