9fd664e6e430cf537a24cee0295597b151e39c42
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-09-22 13:47:44 +0000

    initailised the various state variables for the project

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [breakLength, setBreakLength] = useState();
+  const [sessionLength, setSessionLength] = useState();
+  const [displayTime, setDisplayTime] = useState(25*60);
+
+
+
   return (
     <main className="container">
       <h1 className="title">Pomodoro Clock</h1>
-      <div className="App">
+      <section className="App">
       
-      </div>
+      </section>
     </main>
   );
 }
