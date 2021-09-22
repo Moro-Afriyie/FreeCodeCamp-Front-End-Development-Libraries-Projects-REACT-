408eeb94b846be5a6b5d7b6d706f219ff037db63
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-09-22 13:58:19 +0000

    created a function that converts the seconds into standard time format

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -4,15 +4,19 @@ import {useState} from 'react';
 function App() {
   const [breakLength, setBreakLength] = useState();
   const [sessionLength, setSessionLength] = useState();
-  const [displayTime, setDisplayTime] = useState(25*60);
+  const [displayTime, setDisplayTime] = useState(25*60); // 25 minutes
 
-
-
+  const formatTime = (time)=>{
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return `${minutes> 10 ? minutes : "0" + minutes}:${seconds> 10 ? seconds : "0" + seconds}` 
+  }
+ 
   return (
     <main className="container">
       <h1 className="title">Pomodoro Clock</h1>
       <section className="App">
-      
+       <h1>{formatTime(displayTime)}</h1>
       </section>
     </main>
   );
